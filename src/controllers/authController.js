e5ede93fc49d472d0511772482dc1edebffeea2d
thinkfeatSS <thinkfeat@gmail.com>
2024-10-19 10:04:47 +0500

    fixed

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -24,7 +24,7 @@ exports.register = async (req, res) => {
 
     if (existingUser.length > 0) {
       // If a user is found with the provided email, return an error
-      return res.status(409).json({ message: "User already exists" });
+      return res.status(409).json({ error: "User already exists" });
     }
     // Step 2: If the user doesn't exist, hash the password
     const hashedPassword = await bcrypt.hash(password, 10);
@@ -33,7 +33,7 @@ exports.register = async (req, res) => {
       "INSERT INTO users (username, email, password_hash) VALUES (?, ?, ?)",
       [username, email, hashedPassword]
     );
-    res.status(201).json({ error: "User registered successfully" });
+    res.status(201).json({ message: "User registered successfully" });
   } catch (error) {
     res.status(500).json({ error: "User registration failed" });
   }
