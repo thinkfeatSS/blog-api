b399474d83f634585d5456e894309f2989309e6f
thinkfeatSS <thinkfeat@gmail.com>
2024-10-19 13:03:57 +0500

    fixed

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -6,6 +6,6 @@ const router = express.Router();
 router.post('/register', register);
 router.post('/login', login);
 router.post('/logout', logout);
-router.post('/me',protect, me);
+router.get('/me',protect, me);
 
 module.exports = router;
