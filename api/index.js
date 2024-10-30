e87f0f6c6e34e7d7992bbfb662d5e402b3820f8d
thinkfeatSS <thinkfeat@gmail.com>
2024-10-30 12:53:47 +0500

    create post

--- api/index.js
+++ api/index.js
@@ -40,8 +40,8 @@ const corsOptions = {
 };
 app.use(cors(corsOptions));
 // Middleware to parse JSON requests
-app.use(express.json({ limit: '50mb' }));
-app.use(express.urlencoded({limit: '50mb', extended: true}));
+app.use(express.json({ limit: '500mb' }));
+app.use(express.urlencoded({limit: '500mb', extended: true}));
 app.use(compression());
 app.use(helmet());
 // app.use(limiter);
